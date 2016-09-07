4e5f2559fbb666cc673e0faa02df804253f8d2ac
Ed Asriyan <ed-asriyan@protonmail.com>
2016-09-08 00:55:52 +0300

    Created first solution structure

---
+++ Programming/Seminars/2/ConvexHull.h
@@ -0,0 +1,5 @@
+#ifndef __CONVEX_HULL_H__
+#define __CONVEX_HULL_H__
+
+
+#endif // __CONVEX_HULL__
---
+++ Programming/Seminars/2/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "ConvexHull.h"
+
+int main(void)
+{
+
+    return 0;
+}
