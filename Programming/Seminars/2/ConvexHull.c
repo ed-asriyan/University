765a63a26ea3f3a7b623cbcc981db685bb717878
Ed Asriyan <ed-asriyan@protonmail.com>
2016-09-09 21:56:24 +0300

    Added Points comparing function

--- Programming/Seminars/2/ConvexHull.c
+++ Programming/Seminars/2/ConvexHull.c
@@ -1,1 +1,19 @@
 #include "ConvexHull.h"
+
+// comparing two points
+int point_cmp(Point* p1, Point* p2)
+{
+    if (p1->y < p2->y) {
+        return -1;
+    } else if (p1->y == p2->y) {
+        if (p1->x < p2->x) {
+            return -1;
+        } else if (p1->x == p2->x) {
+            return 0;
+        } else {
+            return 1;
+        }
+    } else {
+        return 1;
+    }
+}
