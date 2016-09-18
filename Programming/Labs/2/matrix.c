173e170d677bf8c627189f10ca54ff48be91bfdf
Ed Asriyan <ed-asriyan@protonmail.com>
2016-09-18 23:59:44 +0300

    Added matrix getters/setters

--- Programming/Labs/2/matrix.c
+++ Programming/Labs/2/matrix.c
@@ -1,5 +1,54 @@
 #include "matrix.h"
 
+// set the new matrix size
+// if operation completed returns 0
+// in another way returns -1
+int set_size_matrix(Matrix* matrix, int height, int width)
+{
+	if (height < 0 || width < 0) {
+		fprintf(stderr, "set_matrix_size: size is negative (%d, %d)\n", height, width);
+		return -1;
+	}
+
+	int init_height = matrix->height;
+	int init_width = matrix->width;
+	double** init_data = matrix->data;
+
+	if (!height || !width) {
+		matrix->width = 0;
+		matrix->height = 0;
+
+		if (init_data != NULL) {
+			if (init_width) {
+				for (int i = 0; i < init_height; ++i) {
+					free(init_data[i]);
+				}
+			}
+			if (init_height) {
+				free(init_data);
+			}
+
+			matrix->data = NULL;
+		}
+
+		return 0;
+	}
+
+	double** new_data = init_data;
+	if (init_height != height) {
+		matrix->height = height;
+		new_data = matrix->data = (double**)realloc(matrix->data, sizeof(double*) * height);
+	}
+	if (init_width != width) {
+		matrix->width = width;
+		for (int i = 0; i < height; ++i) {
+			new_data[i] = (double*)realloc(new_data[i], sizeof(double) * width);
+		}
+	}
+
+	return 0;
+}
+
 // returns pointer to the new Matrix object
 // if input values is out of range returns NULL
 Matrix* create_matrix(int height, int width)
@@ -9,13 +58,10 @@ Matrix* create_matrix(int height, int width)
 	}
 
 	Matrix* result = (Matrix*)malloc(sizeof(Matrix));
+	result->data = NULL;
 
-	result->height = height;
-	result->width = width;
-	double** data = result->data = (double**)malloc(sizeof(double*) * height);
-	for (int i = 0; i < height; ++i) {
-		data[i] = (double*)calloc(width, sizeof(double));
-	}
+	set_size_matrix(result, height, width);
+	
 
 	return result;
 }
@@ -23,26 +69,8 @@ Matrix* create_matrix(int height, int width)
 // destroys the Matrix object
 void free_matrix(Matrix* matrix)
 {
-	int height = matrix->height;
-	double** data = matrix->data;
-
-	for (int i = 0; i < height; ++i) {
-		free(data[i]);
-	}
-	free(data);
+	set_size_matrix(matrix, 0, 0);
 	free(matrix);
-}
-
-// returns the pointer to the elements by int coordinates
-// if coordinates is out of range returns NULL
-double* get_elem_matrix(Matrix* matrix, int row, int column)
-{
-	if (row < 0 || row >= matrix->width ||
-		column < 0 || column >= matrix->height) {
-		return NULL;
-	}
-
-	return matrix->data[row] + column;
 }
 
 // returns pointer to the new Matrix object,
@@ -55,18 +83,16 @@ Matrix* scan_matrix(FILE* input_stream)
 	int width = height + 1;
 
 	Matrix* result = (Matrix*)malloc(sizeof(Matrix));
-	result->height = 0;
-	result->width = width;
-	double** data = result->data = (double**)malloc(sizeof(double*) * height);
+	result->data = NULL;
+	set_size_matrix(result, height, width);
+
+	double** data = result->data;
 
 	int is_fail = 0;
 	for (int i = 0; i < height; ++i) {
 		if (is_fail) {
 			break;
 		}
-
-		data[i] = (double*)malloc(sizeof(double) * width);
-		++result->height;
 
 		for (int j = 0; j < width; ++j) {
 			if (is_fail) {
@@ -99,4 +125,23 @@ void print_matrix(FILE* output_stream, Matrix* matrix)
 		}
 		fprintf(output_stream, "\n");
 	}
+}
+
+// returns pointer to two-dimensional array
+// which represents a matrix
+double** get_data_matrix(Matrix* matrix)
+{
+	return matrix->data;
+}
+
+// returns matrix height
+int get_height_matrix(Matrix* matrix)
+{
+	return matrix->height;
+}
+
+// returns matrix width
+int get_width_matrix(Matrix* matrix)
+{
+	return matrix->width;
 }
--- Programming/Labs/2/matrix.h
+++ Programming/Labs/2/matrix.h
@@ -12,6 +12,7 @@ typedef struct
 	double** data;
 } Matrix;
 
+
 // returns pointer to the new Matrix object
 // if input values is out of range returns NULL
 Matrix* create_matrix(int heigth, int width);
@@ -19,9 +20,14 @@ Matrix* create_matrix(int heigth, int width);
 // destroys the Matrix object
 void free_matrix(Matrix* matrix);
 
-// returns the pointer to the elements by int coordinates
-// if coordinates is out of range returns NULL
-double* get_elem_matrix(Matrix* matrix, int row, int column);
+// set the new matrix size
+// if operation completed returns 0
+// in another way returns -1
+int set_size_matrix(Matrix* matrix, int height, int width);
+
+// returns pointer to two-dimensional array
+// which represents a matrix
+double** get_data_matrix(Matrix* matrix);
 
 // returns pointer to the new Matrix object,
 // which is read from input_stream
@@ -30,5 +36,11 @@ Matrix* scan_matrix(FILE* input_stream);
 
 // prints the matrix into output_stream
 void print_matrix(FILE* output_stream, Matrix* matrix);
+
+// returns matrix height
+int get_height_matrix(Matrix* matrix);
+
+// returns matrix width
+int get_width_matrix(Matrix* matrix);
 
 #endif // __ED_ASRIYAN_MATRIX_H__
