7d0329a653ac163e332477c6aeab45c1f7dae175
Ed Asriyan <ed-asriyan@protonmail.com>
2016-09-21 20:10:19 +0300

    Filled main.c

--- Programming/Labs/2/main.c
+++ Programming/Labs/2/main.c
@@ -7,16 +7,30 @@ int main()
 {
 	Matrix* m = scan_matrix(stdin);
 	if (m == NULL) {
-		fprintf(stderr, "Matrix read is failed.\n");
-		return -1;
+		fprintf(stderr, "main: Matrix read is failed.\n");
+		return -11;
 	}
 
 	Matrix* result = create_matrix(get_height_matrix(m), 1);
-	solve_gauss(m, result);
-	print_matrix(stdout, result);
+	
+	if (result == NULL) {
+		fprintf(stderr, "main: Result matrix creating is failed\n");
+		return -10;
+	}
+
+	int result_code;
+	if (!(result_code = solve_gauss(m, result))) {
+		if (get_height_matrix(result)) {
+			print_matrix(stdout, result);
+		} else {
+			printf("There aren't any solutions.\n");
+		}
+	} else {
+		fprintf(stderr, "main: Solving equation by Gauss method is failed\n");
+	}
 
 	free_matrix(result);
 	free_matrix(m);
 
-	return 0;	
+	return result_code;	
 }
