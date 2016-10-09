4c8851199931a59c467f9bbafdd1d1f59678421e
Ed Asriyan <ed-asriyan@protonmail.com>
2016-10-10 01:13:08 +0300

    Created XML parser

---
+++ Programming/Labs/3/XML/xml_object.h
@@ -0,0 +1,25 @@
+//
+// Created by ed on 09.10.16.
+//
+
+#ifndef XMLCUSTOMERS_XMLOBJECT_H
+#define XMLCUSTOMERS_XMLOBJECT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+	char* tag_name;
+	char* body;
+} XmlObject;
+
+XmlObject* ParseXml(char*, char** xml_end);
+
+void freeXmlObject(XmlObject*);
+
+char* get_xml_object_tag_name(XmlObject*);
+
+char* get_xml_object_body(XmlObject*);
+
+#endif //XMLCUSTOMERS_XMLOBJECT_H
