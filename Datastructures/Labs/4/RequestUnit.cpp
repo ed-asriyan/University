7de86879a0f1fb32c0e31cd9083475e540306877
Ed Asriyan <ed-asriyan@protonmail.com>
2016-11-02 23:24:24 +0300

    Set RequestUnit as structure

--- Datastructures/Labs/4/RequestUnit.cpp
+++ Datastructures/Labs/4/RequestUnit.cpp
@@ -4,24 +4,4 @@
 
 #include "RequestUnit.h"
 
-int RequestUnit::get_id() const {
-	return id;
-}
-
-int RequestUnit::get_n_proc() const {
-	return proc_count;
-}
-
-double RequestUnit::get_income_time() const {
-	return income_time;
-}
-
-double RequestUnit::get_outcome_time() const {
-	return outcome_time;
-}
-
-double RequestUnit::get_querring_time() const {
-	return querring_time;
-}
-
 RequestUnit::RequestUnit(int id) : id(id) {}
--- Datastructures/Labs/4/RequestUnit.h
+++ Datastructures/Labs/4/RequestUnit.h
@@ -5,24 +5,16 @@
 #ifndef QUEUE_UNITREQUEST_H
 #define QUEUE_UNITREQUEST_H
 
-class RequestUnit {
-	private:
-		int id = 0;
-		int proc_count = 0;
+struct RequestUnit {
+	int id = 0;
+	int proc_count = 0;
 
-		double income_time = 0;
-		double outcome_time = 0;
-		double querring_time = 0;
+	double income_time = 0;
+	double outcome_time = 0;
+	double querring_time = 0;
 
-	public:
-		RequestUnit() = default;
-		RequestUnit(int id);
-
-		int get_id() const;
-		int get_n_proc() const;
-		double get_income_time() const;
-		double get_outcome_time() const;
-		double get_querring_time() const;
+	RequestUnit() = default;
+	RequestUnit(int id);
 };
 
 #endif //QUEUE_UNITREQUEST_H
