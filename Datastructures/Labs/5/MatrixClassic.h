49c558c288aa40a82c403fd6534a350a3b623757
Ed Asriyan <ed-asriyan@protonmail.com>
2016-11-20 21:00:10 +0300

    Improve the classic matrix allocator

--- Datastructures/Labs/5/MatrixClassic.h
+++ Datastructures/Labs/5/MatrixClassic.h
@@ -95,6 +95,9 @@ namespace Matrix {
 		data = new T* [height];
 		for (int i = 0; i < height; ++i) {
 			data[i] = new T[width];
+			for (int j = 0; j < width; ++j) {
+				data[i][j] = T();
+			}
 		}
 		_height = height;
 	}
