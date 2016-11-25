7025d4b6db5b53a3baf07de451a1254227e49c1b
Ed Asriyan <ed-asriyan@protonmail.com>
2016-11-25 18:20:26 +0300

    Create optimized sparse matrix

--- Datastructures/Labs/5/SparseMatrixOptimized.h
+++ Datastructures/Labs/5/SparseMatrixOptimized.h
@@ -2,8 +2,8 @@
 // Created by ed on 20.11.16.
 //
 
-#ifndef MATRIX_SPARSEMATRIX_H
-#define MATRIX_SPARSEMATRIX_H
+#ifndef MATRIX_SPARSEMATRIXOPTIMIZED_H
+#define MATRIX_SPARSEMATRIXOPTIMIZED_H
 
 #include <vector>
 #include <list>
@@ -13,33 +13,28 @@
 
 namespace Matrix {
 	/**
-	 * @brief Sparse matrix
+	 * @brief [Self-]optimized sparse matrix
 	 */
-	template<class T>
-	class SparseMatrix : public MatrixBase<T> {
+	template<class T, T neutral_elem = T()>
+	class SparseMatrixOptimized : public MatrixBase<T> {
 		private:
 			T neutral_elem = T();
 
 			/**
-			 * Non-zero elements.
-			 */
-			std::vector<T> data;
-
-			/**
-			 * Row element type.
-			 */
+             * Row element type.
+             */
 			struct RowItem {
-				unsigned long index;
+				T value;
 				int row;
 			};
 
 			/**
-			 * List of iterator to the item in data.
+			 * List of iterator to the item in data
 			 */
 			std::list<RowItem> rows;
 
 			/**
-			 * Vector of iterator to the first row element of the i column/
+			 * Vector of iterator to the first row element of the i column
 			 */
 			std::vector<decltype(rows.begin())> columns;
 
@@ -52,12 +47,12 @@ namespace Matrix {
 			 * @param height Height of the matrix.
 			 * @param width Width of hte matrix.
 			 */
-			SparseMatrix(int height, int width);
+			SparseMatrixOptimized(int height, int width);
 
 			/**
 			 * @brief Copy constructor.
 			 */
-			SparseMatrix(const SparseMatrix<T>&);
+			SparseMatrixOptimized(const SparseMatrixOptimized<T>&);
 
 			/**
 			 * @brief Size of the instance in bytes.
@@ -65,10 +60,10 @@ namespace Matrix {
 			 */
 			virtual auto get_size() const -> decltype(sizeof(T));
 
-			SparseMatrix<T>& operator=(const SparseMatrix<T>&);
+			SparseMatrixOptimized<T>& operator=(const SparseMatrixOptimized<T>&);
 
-			SparseMatrix<T> operator+(const SparseMatrix<T>&) const;
-			SparseMatrix<T>& operator+=(const SparseMatrix<T>&);
+			SparseMatrixOptimized<T> operator+(const SparseMatrixOptimized<T>&) const;
+			SparseMatrixOptimized<T>& operator+=(const SparseMatrixOptimized<T>&);
 
 		protected:
 			virtual void _set_item(int row, int col, const T& value) override;
@@ -77,26 +72,26 @@ namespace Matrix {
 	};
 
 	template<class T>
-	SparseMatrix<T>::SparseMatrix(int height, int width) : MatrixBase<T>(height, width) {
+	SparseMatrixOptimized<T>::SparseMatrixOptimized(int height, int width) : MatrixBase<T>(height, width) {
 		_resize();
 	}
 
 	template<class T>
-	SparseMatrix<T>::SparseMatrix(const SparseMatrix<T>& matrix) : SparseMatrix<T>(matrix.get_height(),
-	                                                                               matrix.get_width()) {
+	SparseMatrixOptimized<T>::SparseMatrixOptimized(const SparseMatrixOptimized<T>& matrix) : SparseMatrixOptimized<T>(
+		matrix.get_height(),
+		matrix.get_width()) {
 		*this = matrix;
 	}
 
 	template<class T>
-	void SparseMatrix<T>::_set_item(int row, int col, const T& value) {
+	void SparseMatrixOptimized<T>::_set_item(int row, int col, const T& value) {
 		auto it = findRowIterator(row, col);
 		if (it != rows.end()) {
-			data[it->index] = value;
+			it->value = value;
 		} else {
 			if (value != neutral_elem) {
 				auto row_ref = columns[col];
-				auto new_it = rows.insert(row_ref, RowItem{data.size(), row});
-				data.push_back(value);
+				auto new_it = rows.insert(row_ref, RowItem{value, row});
 				for (int i = col - 1; i >= 0; --i) {
 					if (columns[i] == row_ref) {
 						columns[i] = new_it;
@@ -110,13 +105,13 @@ namespace Matrix {
 	}
 
 	template<class T>
-	const T& SparseMatrix<T>::_get_item(int row, int col) const {
+	const T& SparseMatrixOptimized<T>::_get_item(int row, int col) const {
 		const auto it = findRowIterator(row, col);
-		return it != rows.end() ? data[it->index] : neutral_elem;
+		return it != rows.end() ? it->value : neutral_elem;
 	}
 
 	template<class T>
-	void SparseMatrix<T>::_resize() {
+	void SparseMatrixOptimized<T>::_resize() {
 		rows.clear();
 		auto rows_end = rows.end();
 		columns.resize((unsigned long) MatrixBase<T>::get_width() + 1);
@@ -126,7 +121,7 @@ namespace Matrix {
 	}
 
 	template<class T>
-	SparseMatrix<T>& SparseMatrix<T>::operator+=(const SparseMatrix<T>& b) {
+	SparseMatrixOptimized<T>& SparseMatrixOptimized<T>::operator+=(const SparseMatrixOptimized<T>& b) {
 		const int height = MatrixBase<T>::get_height();
 		const int width = MatrixBase<T>::get_width();
 		if (height != b.get_height() || width != b.get_width()) {
@@ -144,24 +139,23 @@ namespace Matrix {
 	}
 
 	template<class T>
-	SparseMatrix<T> SparseMatrix<T>::operator+(const SparseMatrix<T>& b) const {
-		SparseMatrix<T> result = *this;
+	SparseMatrixOptimized<T> SparseMatrixOptimized<T>::operator+(const SparseMatrixOptimized<T>& b) const {
+		SparseMatrixOptimized<T> result = *this;
 		add(*this, b, result);
 		return result;
 	}
 
 	template<class T>
-	auto SparseMatrix<T>::get_size() const -> decltype(sizeof(T)) {
+	auto SparseMatrixOptimized<T>::get_size() const -> decltype(sizeof(T)) {
 		return \
             sizeof(*this) + // struct size
-			sizeof(*(data.begin())) * data.size() + // data items
 			sizeof(*(rows.begin())) * rows.size() + // rows items
 			sizeof(*(columns.begin())) * columns.size() + // columns size
 			0;
 	}
 
 	template<class T>
-	auto SparseMatrix<T>::findRowIterator(int row, int col) -> decltype(rows.begin()) {
+	auto SparseMatrixOptimized<T>::findRowIterator(int row, int col) -> decltype(rows.begin()) {
 		auto next_col = columns[col + 1];
 		for (auto it = columns[col]; it != next_col; ++it) {
 			if (it->row == row) {
@@ -172,7 +166,7 @@ namespace Matrix {
 	}
 
 	template<class T>
-	auto SparseMatrix<T>::findRowIterator(int row, int col) const -> decltype(rows.cbegin()) {
+	auto SparseMatrixOptimized<T>::findRowIterator(int row, int col) const -> decltype(rows.cbegin()) {
 		typename std::list<RowItem>::const_iterator next_col = columns[col + 1];
 		for (typename std::list<RowItem>::const_iterator it = columns[col]; it != next_col; ++it) {
 			if (it->row == row) {
@@ -183,7 +177,7 @@ namespace Matrix {
 	}
 
 	template<class T>
-	SparseMatrix<T>& SparseMatrix<T>::operator=(const SparseMatrix<T>& matrix) {
+	SparseMatrixOptimized<T>& SparseMatrixOptimized<T>::operator=(const SparseMatrixOptimized<T>& matrix) {
 		const int height = matrix.get_height();
 		const int width = matrix.get_width();
 
@@ -192,7 +186,7 @@ namespace Matrix {
 		for (int i = 0; i < width; ++i) {
 			auto next_row = matrix.columns[i + 1];
 			for (auto bt = matrix.columns[i]; bt != next_row; ++bt) {
-				_set_item(bt->row, i, matrix.data[bt->index]);
+				_set_item(bt->row, i, bt->value);
 			}
 		}
 
@@ -200,4 +194,4 @@ namespace Matrix {
 	}
 }
 
-#endif //MATRIX_SPARSEMATRIX_H
+#endif //MATRIX_SPARSEMATRIXOPTIMIZED_H
