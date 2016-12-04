33842820aa39568cea610b3fff0dcde13f3cfe44
Ed Asriyan <ed-asriyan@protonmail.com>
2016-12-04 21:37:15 +0300

    Support multidigit numbers

--- Programming/Labs/8/main.c
+++ Programming/Labs/8/main.c
@@ -101,6 +101,28 @@ int IsIn(char ch, char* alphabet) {
 	return -1;
 }
 
+/* return a new string with every instance of ch replaced by repl */
+char* replace(const char* s, char ch, const char* repl) {
+	int count = 0;
+	const char* t;
+	for (t = s; *t; t++)
+		count += (*t == ch);
+
+	size_t rlen = strlen(repl);
+	char* res = malloc(strlen(s) + (rlen - 1) * count + 1);
+	char* ptr = res;
+	for (t = s; *t; t++) {
+		if (*t == ch) {
+			memcpy(ptr, repl, rlen);
+			ptr += rlen;
+		} else {
+			*ptr++ = *t;
+		}
+	}
+	*ptr = 0;
+	return res;
+}
+
 int main() {
 	CharStack* expression = Constructor();
 	char current_symbol;
@@ -123,18 +145,25 @@ int main() {
 	int k = 0;
 
 	for (int i = 0; i < Size(expression); ++i) {
-
 		if (isalpha(expression->base[i])) {
+
 			int code = IsIn(expression->base[i], vars);
 
 			if (code == -1) {
 				fprintf(stdout, "%c = ", expression->base[i]);
 				int temp;
 				fscanf(stdin, "%d", &temp);
+
+				char tmp[100];
+				sprintf(tmp, "%d", temp);
+				char* e = replace(expression->base, expression->base[i], tmp);
 				vars[k] = expression->base[i];
 				vars[k + 1] = '\0';
 				values[k] = temp;
-				expression->base[i] = temp + '0';
+
+				free(expression->base);
+				expression->base = e;
+				expression->stack_size = expression->base_size = strlen(e);
 				++k;
 			} else {
 				expression->base[i] = values[code] + '0';
