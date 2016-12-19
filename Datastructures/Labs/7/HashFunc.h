eb2de639a8e4bbd2ba449c88b1147e20e0f951b2
Ed Asriyan <ed-asriyan@protonmail.com>
2016-12-19 22:59:34 +0300

    Update hash func

--- Datastructures/Labs/7/HashFunc.h
+++ Datastructures/Labs/7/HashFunc.h
@@ -1,16 +1,25 @@
 //
 // Created by lieroz on 14.12.16.
+// Modified by ed on 19.12.16.
 //
 
 #ifndef LAB_07_HASHFUNC_H
 #define LAB_07_HASHFUNC_H
 
 #include <cstddef>
+#include <random>
 
 template<class K>
 struct HashFunc {
 	size_t operator()(const K& key, const size_t range) const {
-		return (key) % range;
+		if (sizeof(K) < sizeof(unsigned int)) {
+			const char* ptr = (const char*) &key;
+			std::srand(static_cast<unsigned int>(*ptr));
+		} else {
+			const unsigned int* ptr = (const unsigned int*) &key;
+			std::srand(*ptr);
+		}
+		return rand() % range;
 	}
 };
 
