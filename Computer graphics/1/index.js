57584e9e9a3abcbbf234d56863342c720eeda1b2
Ed Asriyan <ed-asriyan@protonmail.com>
2017-02-23 21:26:07 +0300

    Update canvasManager interface

--- Computer graphics/1/index.js
+++ Computer graphics/1/index.js
@@ -4,75 +4,87 @@
  * Created by ed on 19.02.17.
  */
 
-const body = document.getElementById('body');
 const mainCanvas = document.getElementById('mainCanvas');
 const mousePositionLabel = document.getElementById('mousePosition');
-const pointList = document.getElementById('pointsList');
-const pointsNumber = document.getElementById('pointsNumber');
 
 mainCanvas.height = mainCanvas.parentNode.clientHeight;
 mainCanvas.width = mainCanvas.parentNode.clientWidth;
 
 const canvasManager = new CanvasManager(mainCanvas);
-canvasManager.drawGrid({
-    gridStep: 20,
-    gridColor: 'gray',
-    gridBorder: true,
-});
 
 const getCurrentHistoryState = function () {
-    return {
-        points: pointList.innerHTML,
-        pointsNumber: pointsNumber.innerHTML,
-        canvas: canvasManager.getImageData(),
-    }
+    return canvasManager.points.slice();
 };
 
 const setCurrentHistoryState = function (state) {
     if (state) {
-        pointList.innerHTML = state.points;
-        pointsNumber.innerHTML = state.pointsNumber;
-        canvasManager.putImageData(state.canvas);
+        canvasManager.points = state;
     }
+    reDraw();
+};
+
+const updatePointsList = function () {
+    let pointsTable = document.getElementById('pointsList');
+
+    // remove children
+    while (pointsTable.firstChild) {
+        pointsTable.removeChild(pointsTable.firstChild);
+    }
+
+    canvasManager.points.forEach(function (point, index) {
+        let row = document.createElement('tr');
+        let x = point.x.valueOf();
+        let y = point.y.valueOf();
+        index = index.valueOf();
+
+        row.innerHTML = `<td>${index + 1}</td><td>${x}</td><td>${y}</td>`;
+        row.onclick = function () {
+            canvasManager.points.splice(index, 1);
+            capture();
+            reDraw();
+        };
+        pointsTable.appendChild(row);
+    });
+
+    document.getElementById('pointsNumber').innerHTML = pointList.childNodes.length;
 };
 
 const historyManager = new HistoryManager(getCurrentHistoryState());
 
-const addPoint = function (x, y) {
-    canvasManager.drawPoint(x, y, {
-        width: 5,
-        color: 'red'
-    });
-
-    let pointRow = document.createElement('tr');
-    pointRow.className = '';
-    pointRow.innerHTML = `<td>${pointList.childNodes.length + 1}</td><td>${x}</td><td>${y}</td>`;
-
-    pointList.appendChild(pointRow);
-    pointsNumber.innerHTML = pointList.childNodes.length;
-
-    historyManager.capture(getCurrentHistoryState());
+const reDraw = function () {
+    canvasManager.reDraw();
+    updatePointsList();
 };
 
-const undoPoint = function () {
-    setCurrentHistoryState(historyManager.undo());
+const capture = function () {
+    historyManager.capture(canvasManager.points.slice());
 };
 
-const redoPoint = function () {
-    setCurrentHistoryState(historyManager.redo());
+const undo = function () {
+    canvasManager.points = historyManager.undo() || canvasManager.points;
+    reDraw();
 };
 
-const resetPoint = function () {
-    setCurrentHistoryState(historyManager.reset());
+const redo = function () {
+    canvasManager.points = historyManager.redo() || canvasManager.points;
+    reDraw();
 };
 
-const clearPoint = function () {
-    setCurrentHistoryState(historyManager.clear());
+const reset = function () {
+    canvasManager.points = historyManager.reset() || canvasManager.points;
+    reDraw();
+};
+
+const clear = function () {
+    canvasManager.points = historyManager.clear() || canvasManager.points;
+    reDraw();
 };
 
 mainCanvas.addEventListener('click', function (e) {
     let pos = canvasManager.getMousePosition(e);
-    addPoint(pos.x, pos.y);
+    canvasManager.addPoint(pos.x, pos.y);
+    capture();
+    reDraw();
 });
 
 mainCanvas.addEventListener('mousemove', function (e) {
--- Computer graphics/canvasManager.js
+++ Computer graphics/canvasManager.js
@@ -10,6 +10,40 @@ class CanvasManager {
         }
 
         this._canvas = canvas;
+        this._points = [];
+
+        this._grid = options.grid ||
+            {
+                xStep: 10,
+                yStep: 10,
+                color: 'gray',
+                border: true,
+                visible: true,
+            };
+
+        this._pointOptions = options.pointOptions ||
+            {
+                color: 'black',
+                radius: 2.5
+            };
+
+        this._drawGrid();
+    }
+
+    get points() {
+        return this._points;
+    }
+
+    set points(value) {
+        this._points = value;
+    }
+
+    get pointOptions() {
+        return this._pointOptions;
+    }
+
+    set pointOptions(value) {
+        this._pointOptions = value;
     }
 
     getMousePosition(event) {
@@ -33,7 +67,6 @@ class CanvasManager {
         context.beginPath();
 
         context.lineWidth = options.lineWidth || 1;
-        context.strokeStyle = options.color || 'black';
         context.strokeOpacity = options.opacity || 1;
 
         context.moveTo(x1, y1);
@@ -43,20 +76,12 @@ class CanvasManager {
         context.closePath();
     }
 
-    drawPoint(x, y, options) {
-        if (!options) {
-            options = {}
-        }
-
-        let context = this._canvas.getContext('2d');
-        context.beginPath();
-
-        context.fillStyle = context.strokeStyle = options.color || 'black';
-
-        context.arc(x, y, options.width / 2 || 1, 0, 2 * Math.PI, false);
-
-        context.fill();
-        context.closePath();
+    addPoint(x, y, color) {
+        this._points.push({
+            x: x,
+            y: y,
+            color: color || 'black'
+        });
     }
 
     getImageData() {
@@ -67,45 +92,55 @@ class CanvasManager {
     putImageData(src) {
         if (src) {
             let ctx = this._canvas.getContext('2d');
-            // let data = JSON.parse(src);
             ctx.putImageData(src, 0, 0);
         }
     }
 
-    drawGrid(options) {
-        if (!options) {
-            options = {};
-        }
-
-        let gridXStep = options.gridXStep || options.gridYStep || options.gridStep || 10;
-        let gridYStep = options.gridYStep || options.gridXStep || options.gridStep || 10;
-        let gridColor = options.gridColor || 'gray';
-        let gridOpacity = options.gridOpacity || 1;
-        let gridBorder = options.gridBorder || (options.gridBorder == undefined);
+    _drawGrid() {
+        if (!this._grid.visible) return;
 
         let context = this._canvas.getContext("2d");
         context.beginPath();
-        for (let x = 0.5; x <= this._canvas.width; x += gridXStep) {
+        for (let x = 0.5; x <= this._canvas.width; x += this._grid.xStep) {
             context.moveTo(x, 0);
             context.lineTo(x, this._canvas.height);
         }
 
-        for (let y = 0.5; y <= this._canvas.height; y += gridYStep) {
+        for (let y = 0.5; y <= this._canvas.height; y += this._grid.yStep) {
             context.moveTo(0, y);
             context.lineTo(this._canvas.width, y);
         }
 
-        if (gridBorder) {
+        if (this._grid.border) {
             context.moveTo(this._canvas.width - 0.5, 0.5);
             context.lineTo(this._canvas.width - 0.5, this._canvas.height - 0.5);
             context.moveTo(0.5, this._canvas.height - 0.5);
             context.lineTo(this._canvas.width - 0.5, this._canvas.height - 0.5);
         }
 
-        context.strokeStyle = gridColor;
-        context.strokeOpacity = gridOpacity;
+        context.strokeStyle = this._grid.color;
         context.stroke();
 
         context.closePath();
+    }
+
+    reDraw() {
+        let context = this._canvas.getContext("2d");
+        context.clearRect(0, 0, this._canvas.width, this._canvas.height);
+
+        this._drawGrid();
+
+        context = this._canvas.getContext("2d");
+        for (let p in this._points) {
+            let point = this._points[p];
+
+            if (!point.x || !point.y) continue;
+
+            context.beginPath();
+            context.fillStyle = context.strokeStyle = point.color;
+            context.arc(point.x, point.y, this._pointOptions.radius, 0, 2 * Math.PI);
+            context.fill();
+            context.closePath();
+        }
     }
 }
