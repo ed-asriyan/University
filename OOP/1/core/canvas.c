83eeb616ed77774861b5c7c4e837db9f5c5f6f9c
Ed Asriyan <ed-asriyan@protonmail.com>
2017-03-07 19:28:52 +0300

    Create canvas interface

---
+++ OOP/1/core/canvas.c
@@ -0,0 +1,19 @@
+//
+// Created by ed on 07.03.17.
+//
+
+#include "canvas.h"
+
+unsigned get_width(const canvas_t* canvas) {
+	// todo:
+	return 100;
+}
+
+unsigned get_height(const canvas_t* canvas) {
+	// todo:
+	return 100;
+}
+
+void draw_line(canvas_t* canvas, const vector_t* point_a, const vector_t* point_b) {
+	// todo:
+}
---
+++ OOP/1/core/canvas.h
@@ -0,0 +1,16 @@
+//
+// Created by ed on 07.03.17.
+//
+
+#ifndef INC_3DVIEW_CANVAS_H
+#define INC_3DVIEW_CANVAS_H
+
+#include "math.h"
+
+typedef struct canvas canvas_t;
+
+unsigned get_width(const canvas_t* canvas);
+unsigned get_height(const canvas_t* canvas);
+void draw_line(canvas_t* canvas, const vector_t* point_a, const vector_t* point_b);
+
+#endif //INC_3DVIEW_CANVAS_H
