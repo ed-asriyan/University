2b820609522f4c51331ad9b7c32c84ecdd0a8d6d
Ed Asriyan <ed-asriyan@protonmail.com>
2017-03-11 23:43:21 +0300

    Add circle sequence

--- Computer graphics/2/solver.js
+++ Computer graphics/2/solver.js
@@ -15,13 +15,29 @@ class Solver {
         return new Point(x, y);
     };
 
+    circle(t, radius = this.b / 4) {
+        let x = radius * Math.cos(t);
+        let y = radius * Math.sin(t);
+        return new Point(x, y);
+    }
+
     *astroidSequence(options = {left: 0, right: 2 * Math.PI, step: 1e-2}) {
         for (let t = options.left; t < options.right; t += options.step) {
             yield this.astroid(t);
         }
     };
 
+    *circleSequence(options = {radius: this.b / 4, centerPoint: true, step: 1e-2}) {
+        for (let t = 0; t < Math.PI * 2; t += options.step) {
+            yield this.circle(t, options.radius);
+        }
+        if (options.centerPoint) {
+            yield new Point(0, 0);
+        }
+    }
+
     *allPointsSequence(options = {}) {
         yield* this.astroidSequence(options.astroid);
+        yield* this.circleSequence(options.circle);
     }
 }
