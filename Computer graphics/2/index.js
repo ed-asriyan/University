8c1f4d061990b874f5424bbba166d90ebecc90cc
Ed Asriyan <ed-asriyan@protonmail.com>
2017-03-12 02:53:05 +0300

    Create interface

--- Computer graphics/2/index.js
+++ Computer graphics/2/index.js
@@ -3,3 +3,87 @@
 /**
  * Created by ed on 11.03.17.
  */
+
+//
+// XML elements
+//
+const mainCanvas = document.getElementById('mainCanvas');
+
+const resetButton = document.getElementById('reset');
+const redrawButton = document.getElementById('redraw');
+
+const xOffsetInput = document.getElementById('xOffset');
+const yOffsetInput = document.getElementById('yOffset');
+const offsetButton = document.getElementById('offsetBtn');
+
+const xScaleInput = document.getElementById('xScale');
+const yScaleInput = document.getElementById('yScale');
+const xScaleCenterInput = document.getElementById('xScaleCenter');
+const yScaleCenterInput = document.getElementById('yScaleCenter');
+const scaleButton = document.getElementById('scaleBtn');
+
+const xRotationCenterInput = document.getElementById('xRotationCenter');
+const yRotationCenterInput = document.getElementById('yRotationCenter');
+const rotationInput = document.getElementById('rotation');
+const rotationButton = document.getElementById('rotationBtn');
+
+const canvasManager = new CanvasManager(mainCanvas);
+const solver = new Solver(100);
+
+//
+// Functions
+//
+const resetOptions = function () {
+    mainCanvas.width = mainCanvas.parentNode.clientWidth;
+
+    xOffsetInput.value = Math.round(mainCanvas.width / 2);
+    yOffsetInput.value = Math.round(mainCanvas.height / 2);
+
+    xScaleInput.value = 1;
+    yScaleInput.value = 1;
+    xScaleCenterInput.value = 0;
+    yScaleCenterInput.value = 0;
+
+    xRotationCenterInput.value = 0;
+    yRotationCenterInput.value = 0;
+    rotationInput.value = 0;
+};
+
+const reDraw = function () {
+    let xOffset = parseInt(xOffsetInput.value);
+    let yOffset = parseInt(yOffsetInput.value);
+
+    let xScale = parseInt(xScaleInput.value);
+    let yScale = parseInt(yScaleInput.value);
+
+    return new Promise(() => {
+        canvasManager.clear();
+
+        canvasManager.offset = new Point(xOffset, yOffset);
+        canvasManager.scale = new Point(xScale, yScale);
+
+        canvasManager.drawGrid();
+        for (let point of solver.allPointsSequence()) {
+            canvasManager.drawPoint({
+                point: point
+            });
+        }
+    });
+};
+
+
+//
+// XML elements initialization
+//
+resetOptions();
+reDraw();
+
+
+//
+// Add routed event listeners
+//
+resetButton.addEventListener('click', resetOptions);
+redrawButton.addEventListener('click', reDraw);
+offsetButton.addEventListener('click', reDraw);
+scaleButton.addEventListener('click', reDraw);
+rotationButton.addEventListener('click', reDraw);
