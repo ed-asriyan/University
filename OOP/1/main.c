001f624cf6c83c73e6212374b9663a89d87fd625
Ed Asriyan <ed-asriyan@protonmail.com>
2017-03-16 00:15:03 +0300

    Add error logging

--- OOP/1/main.c
+++ OOP/1/main.c
@@ -20,6 +20,44 @@ bool mouse_button_left_pressed = false;
 
 bool mouse_button_right_pressed = false;
 
+const char* error_name(error_t error) {
+	const char* msg = NULL;
+	switch (error) {
+		case NONE:
+			msg = "";
+			break;
+		case OUT_OF_MEMORY:
+			msg = "out of memory";
+			break;
+		case NOT_INITIALIZED:
+			msg = "not initialized";
+			break;
+		case ALREADY_INITIALIZED:
+			msg = "already initialized";
+			break;
+		case MODEL_NOT_LOADED:
+			msg = "model not loaded";
+			break;
+		case UNABLE_TO_OPEN_FILE:
+			msg = "unable to open file";
+			break;
+		case INVALID_FILE:
+			msg = "invalid file";
+			break;
+		case UNKNOWN_COMMAND:
+			msg = "unknown";
+			break;
+	}
+	return msg;
+}
+
+error_t log_if_error(const char* action, error_t error) {
+	if (error != NONE) {
+		fprintf(stderr, "Error on %s: %s.\n", action, error_name(error));
+	}
+	return error;
+}
+
 void redraw(void) {
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -29,7 +67,7 @@ void redraw(void) {
 
 	glBegin(GL_LINES);
 	command_data_t command_data;
-	execute(RENDER, &command_data);
+	log_if_error("rendering", execute(RENDER, &command_data));
 
 	glEnd();
 	glFlush();
@@ -63,22 +101,23 @@ void mouse_right_handler(int state, int x, int y) {
 
 void mouse_wheel_handler(int state, int x, int y) {
 	const double factor = 0.9;
+	bool ok = true;
 	command_data_t command_data;
 	switch (state) {
 		case GLUT_DOWN:
-			command_data.transform_object_data.transformation.type = UNIFORM_SCALING;
 			command_data.transform_object_data.transformation.uniform_scaling.factor = factor;
-			execute(TRANSFORM_OBJECT, &command_data);
-			redraw();
 			break;
 		case GLUT_UP:
-			command_data.transform_object_data.transformation.type = UNIFORM_SCALING;
 			command_data.transform_object_data.transformation.uniform_scaling.factor = 1.0 / factor;
-			execute(TRANSFORM_OBJECT, &command_data);
-			redraw();
 			break;
 		default:
+			ok = false;
 			break;
+	}
+	if (ok) {
+		command_data.transform_object_data.transformation.type = UNIFORM_SCALING;
+		log_if_error("object transformation", execute(TRANSFORM_OBJECT, &command_data));
+		redraw();
 	}
 }
 
@@ -116,25 +155,23 @@ void mouse_move_handler(int x, int y) {
 		int dx = x - mouse_position.x;
 		int dy = y - mouse_position.y;
 
-		if (dx || dy) {
+		if (dx || dy && (mouse_button_right_pressed || mouse_button_left_pressed)) {
+			command_data_t command_data;
 			if (mouse_button_left_pressed) {
-				command_data_t command_data;
 				command_data.transform_object_data.transformation.type = ROTATION;
 				command_data.transform_object_data.transformation.rotation.axis_x = -dy;
 				command_data.transform_object_data.transformation.rotation.axis_y = dx;
 				command_data.transform_object_data.transformation.rotation.axis_z = 0.0;
 				command_data.transform_object_data.transformation.rotation.angle = to_radians(1.5);
-				execute(TRANSFORM_OBJECT, &command_data);
-				redraw();
+				log_if_error("object transformation", execute(TRANSFORM_OBJECT, &command_data));
 			} else if (mouse_button_right_pressed) {
-				command_data_t command_data;
 				command_data.transform_camera_data.transformation.type = TRANSLATION;
 				command_data.transform_camera_data.transformation.translation.displacement_x = -dx * 0.01;
 				command_data.transform_camera_data.transformation.translation.displacement_y = -dy * 0.01;
 				command_data.transform_camera_data.transformation.translation.displacement_z = 0.0;
-				execute(TRANSFORM_CAMERA, &command_data);
-				redraw();
+				log_if_error("camera transformation", execute(TRANSFORM_CAMERA, &command_data));
 			}
+			redraw();
 		}
 	}
 
@@ -172,8 +209,7 @@ int main(int argc, char** argv) {
 
 	// initialize core
 	command_data_t command_data;
-	if (execute(STARTUP, &command_data) != NONE) {
-		fprintf(stderr, "Can not startup core\n");
+	if (log_if_error("startup", execute(STARTUP, &command_data)) != NONE) {
 		return -10;
 	}
 
@@ -182,16 +218,14 @@ int main(int argc, char** argv) {
 	command_data.transform_camera_data.transformation.translation.displacement_x = 0.0;
 	command_data.transform_camera_data.transformation.translation.displacement_y = 0.0;
 	command_data.transform_camera_data.transformation.translation.displacement_z = 10.0;
-	if (execute(TRANSFORM_CAMERA, &command_data) != NONE) {
-		fprintf(stderr, "Can not transform camera\n");
+	if (log_if_error("camera transformation", execute(TRANSFORM_CAMERA, &command_data)) != NONE) {
 		return -11;
 	}
 
 	// load from file
 	command_data_t load_command_data;
 	load_command_data.load_model_data.file_path = argv[1];
-	if (execute(LOAD_MODEL, &load_command_data) != NONE) {
-		fprintf(stderr, "Can not load model from %s\n", load_command_data.load_model_data.file_path);
+	if (log_if_error("model loading", execute(LOAD_MODEL, &load_command_data)) != NONE) {
 		return -12;
 	}
 
@@ -200,7 +234,7 @@ int main(int argc, char** argv) {
 
 	// shutdown the core
 	command_data_t shutdown_command_data;
-	execute(SHUTDOWN, &shutdown_command_data);
+	log_if_error("shutdown", execute(SHUTDOWN, &shutdown_command_data));
 
 	return 0;
 }
