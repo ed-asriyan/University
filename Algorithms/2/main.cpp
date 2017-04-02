30a8e6398bd9ebbb1defc4e0cbfd6cfa7685ebe8
Ed Asriyan <ed-asriyan@protonmail.com>
2017-04-02 12:55:41 +0300

    Implement Newton algorithm

--- Algorithms/2/main.cpp
+++ Algorithms/2/main.cpp
@@ -1,10 +1,80 @@
 #include <iostream>
+#include <iomanip>
 
 #include "math_functions.hpp"
 #include "solver.hpp"
 
+class InputValues {
+	public:
+		InputValues()
+			: InputValues(std::cin, std::cout) {}
+
+		InputValues(std::istream& in, std::ostream& out) {
+			out << "Enter left  border: ";
+			in >> x_begin;
+			out << "Enter right border: ";
+			in >> x_end;
+			out << "Enter step: ";
+			in >> x_step;
+			out << "Enter x: ";
+			in >> x;
+			out << "Enter polynomial degree: ";
+			in >> degree;
+		}
+
+		InputValues(int argc, const char* argv[]) {
+			// todo:
+		}
+
+		double get_x_begin() const {
+			return x_begin;
+		}
+
+		double get_x_end() const {
+			return x_end;
+		}
+
+		double get_x_step() const {
+			return x_step;
+		}
+
+		double get_x() const {
+			return x;
+		}
+
+		unsigned int get_degree() const {
+			return degree;
+		}
+
+	private:
+		double x_begin;
+		double x_end;
+		double x_step;
+		double x;
+		unsigned int degree;
+};
+
+void PrintTable(std::ostream& out, const std::vector<Solver::Point>& table) {
+	for (auto& point: table) {
+		out << std::setw(5) << point.x << ": " << point.y << std::endl;
+	}
+}
+
 int main() {
-	auto table = Solver::GenerateTable(Functions::F1, 0, 2, 0.1);
+	InputValues input_values;
+
+	auto table = Solver::GenerateTable(Functions::F1,
+	                                   input_values.get_x_begin(),
+	                                   input_values.get_x_end(),
+	                                   input_values.get_x_step());
+	std::cout << "PointsTable:" << std::endl;
+	PrintTable(std::cout, table);
+
+	auto index = Solver::GetIndexOfInterpolationPoint(table, input_values.get_x());
+	auto indexes = Solver::GetInterpolationPoints(table, index, input_values.get_degree());
+	auto res = Solver::NewtonArrDividedDivision(table, indexes.first, indexes.second, input_values.get_x());
+
+	std::cout << res << std::endl;
 
 	return 0;
 }
--- Algorithms/2/solver.cpp
+++ Algorithms/2/solver.cpp
@@ -4,35 +4,97 @@
 
 #include "solver.hpp"
 
-std::vector<Solver::Point> Solver::GenerateTable(
+Solver::Point::Point(double x, double y)
+	: x(x), y(y) {}
+
+Solver::PointsTable Solver::GenerateTable(
 	const std::function<double(double)>& func,
 	double x_begin,
 	double x_end,
 	double x_step
 ) {
 	int count = static_cast<int>((x_end - x_begin) / x_step) + 1;
-	std::vector<Solver::Point> result(static_cast<unsigned int>(count));
+	Solver::PointsTable result(static_cast<unsigned int>(count));
 	for (int i = 0; i < count; ++i) {
 		double x = x_begin + i * x_step;
-		result[i] = {x, func(x)};
+		result[i].x = x;
+		result[i].y = func(x);
 	}
 	return result;
 }
 
-int Solver::GetIndexofInterpolationPoint(
-	const std::vector<Point>& points,
+size_t Solver::GetIndexOfInterpolationPoint(
+	const Solver::PointsTable& points,
 	double x
 ) {
-	int result = -1;
-	for (int i = 0; i < points.size(); ++i) {
-		if (x < points[i].x) {
+	bool is_found = false;
+	size_t result = 0;
+	for (size_t i = 0; i < points.size() && !is_found; ++i) {
+		if ((is_found = x < points[i].x)) {
 			result = i - 1;
-			break;
 		}
 	}
 
-	if (result == -1 && x > points[points.size() - 1].x) {
-		result = static_cast<int>(points.size() - 1);
+	if (!is_found && x > points[points.size() - 1].x) {
+		result = points.size() - 1;
 	}
 	return result;
+}
+
+std::pair<size_t, size_t> Solver::GetInterpolationPoints(
+	const Solver::PointsTable& table,
+	size_t index,
+	unsigned int degree
+) {
+	unsigned int num_necessary = degree + 1;
+	if (degree >= table.size() - 2) {
+		return std::pair<size_t, size_t>(0, table.size() - 1);
+	}
+
+	int remainder = num_necessary % 2;
+	size_t equalization1 = index - num_necessary / 2 + 1;
+	size_t equalization2 = index + num_necessary / 2 + remainder; // if necessary in odd points number next point is
+	// getting from the end
+	if (equalization1 <= 0) {
+		return std::pair<size_t, size_t>(0, degree);
+	}
+	if (equalization2 >= table.size()) {
+		return std::pair<size_t, size_t>(table.size() - num_necessary, table.size() - 1);
+	}
+
+	return std::pair<size_t, size_t>(equalization1, equalization2);
+}
+
+double Solver::NewtonArrDividedDivision(
+	const Solver::PointsTable& table,
+	size_t index1,
+	size_t index2,
+	double x
+) {
+	size_t polynomial_degree = index2 - index1;
+	std::vector<double> dividers;    // for divided differences calculation
+	std::vector<double> X;
+	std::vector<double> differences; // divided differences
+
+	for (size_t i = index1; i <= index2; ++i) {
+		X.push_back(table[i].x);
+		dividers.push_back(table[i].y);
+		differences.push_back(0);
+	}
+	differences[0] = dividers[0];
+	for (size_t j = 1; j <= polynomial_degree; ++j) {
+		for (size_t i = 0; i <= polynomial_degree - j; ++i) {
+			dividers[i] = (dividers[i + 1] - dividers[i]) / (X[i + j] - X[i]);
+			differences[j] = dividers[0];
+		}
+	}
+
+	double polynomial_sum = differences[0];
+	double polynomial_ratio = 1;
+	for (int i = 1; i <= polynomial_degree; ++i) {
+		polynomial_ratio *= (x - X[i - 1]);
+		polynomial_sum += polynomial_ratio * differences[i];
+	}
+
+	return polynomial_sum;
 }
