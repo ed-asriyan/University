8766a331e6ce481aada447dbbd04146169299d81
Ed Asriyan <ed-asriyan@protonmail.com>
2017-04-08 20:53:34 +0300

    Create Functions::Utils

--- Algorithms/2/part2.cpp
+++ Algorithms/2/part2.cpp
@@ -5,15 +5,31 @@
 #include <cmath>
 
 namespace Functions {
-	inline double reverse(double func(double, double), double x, double y) {
-		return func(y, x);
+	namespace Utils {
+		inline auto Reverse(const std::function<double(double, double)>& func) {
+			return [&func](double x, double y) {
+				return func(y, x);
+			};
+		}
+
+		inline auto SectionX(const std::function<double(double, double)>& func, double x) {
+			return [x, func](double y) {
+				return func(x, y);
+			};
+		}
+
+		inline auto SectionY(const std::function<double(double, double)>& func, double y) {
+			return [y, func](double x) {
+				return func(x, y);
+			};
+		}
 	}
 
-	double F1(double x, double y) {
+	inline double F1(double x, double y) {
 		return std::exp(x * x * x - y) - x * x * x * x * x * x + 2 * (y + 1) * (x * x * x) - y * y - 2 * y - 2;
 	}
 
-	double F2(double x, double y) {
+	inline double F2(double x, double y) {
 		return std::exp(2 * std::log(x) - y) + y * std::exp(-y) - std::exp(x * x) * std::log(x * x + y);
 	}
 }
