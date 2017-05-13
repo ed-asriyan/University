de908ed89a2a7e1fa80f6c1dbadb100e8afc1588
Ed Asriyan <ed-asriyan@protonmail.com>
2017-05-13 21:57:56 +0300

    Create subproject

---
+++ OOP/3/SceneViewer/SceneViewer.hpp
@@ -0,0 +1,17 @@
+//
+// Created by ed on 13.05.17.
+//
+
+#ifndef LAB3_SCENEVIEWER_HPP
+#define LAB3_SCENEVIEWER_HPP
+
+namespace SceneViewer {
+	/**
+	 * Facade.
+	 */
+	class SceneViewer {
+
+	};
+}
+
+#endif //LAB3_SCENEVIEWER_HPP
