7ef04f3f44668b89ddfb2ea2b745c80c49a3511c
Ed Asriyan <ed-asriyan@protonmail.com>
2017-05-14 19:43:29 +0300

    Initial commit

---
+++ Algorithms/4/derivative.hpp
@@ -0,0 +1,12 @@
+//
+// Created by ed on 14.05.17.
+//
+
+#ifndef LAB4_DERIVATIVE_HPP
+#define LAB4_DERIVATIVE_HPP
+
+namespace Derivative {
+
+}
+
+#endif //LAB4_DERIVATIVE_HPP
---
+++ Algorithms/4/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+#include "derivative.hpp"
+
+double f(double x) {
+	return std::exp(x);
+}
+
+int main() {
+
+	return 0;
+}
