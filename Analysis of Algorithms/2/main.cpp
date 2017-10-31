31e4290d143db33984a1dc2dc05e900d23959282
Ed Asriyan <ed-asriyan@protonmail.com>
2017-10-31 17:45:38 +0300

    Create Matrix

---
+++ Analysis of Algorithms/2/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include "matrix/Matrix.hpp"
+#include "matrix/MatrixMultiplication.hpp"
+
+int main() {
+	matrix::Matrix<int> a(
+		{
+			{1, 2, 3},
+			{4, 5, 6},
+			{7, 8, 9},
+		}
+	);
+	matrix::Matrix<int> b(
+		{
+			{0,0,1},
+			{0,1,0},
+			{1,0,0},
+		}
+	);
+
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+
+	return 0;
+}
